9a75df66c91d47895607263fc89454b8048e13da
Piyush-shinde <113353125+Piyush-shinde@users.noreply.github.com>
2023-05-10 18:02:30 +0530

    Add files via upload

---
+++ Assignment 1/Q3.c
@@ -0,0 +1,17 @@
+//Program to print 5 to 1 numbers on screen
+#include<stdio.h>
+void Display()
+{
+	int i = 0;
+	i = 5;
+	for(i;i>0;i--)
+	{
+		printf("%d\n",i);
+		//i++;
+	}
+}
+int main()
+{
+	Display();
+	return 0;
+}
